33ea1ef1bdc33912da6ca5d5703792e6b19ebeae
Daniel Santos <67795039+DanielSantosdani@users.noreply.github.com>
2020-08-15 11:22:50 -0300

    Add files via upload

---
+++ Form1Monstro no espaço.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Lugar As Boolean
+        Dim Posicao As Integer
+        Dim Matou As Byte
+        Dim I As Integer
+
+        Private Declare  Function sndPlaySound Lib "winmm.dil" Alias "
+sndPlaySoundA" (ByVal lpszSoundName As String, By Val uFlags As Long) As Long
+
+    End Sub
+End Class
+
